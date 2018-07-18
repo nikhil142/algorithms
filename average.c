beaddc014d0c729f9722cc8b28515896ebdd2ca2
Nikhil <ns1043937@gmail.com>
2018-07-17 22:07:06 -0700

    inital commit

---
+++ average.c
@@ -0,0 +1,42 @@
+// program for finding the average of three numbers.
+
+/*#include<stdio.h>
+#include<conio.h>
+void avg();                                           // prototype
+void main()
+{
+	avg();                                           // function calling
+	getch();
+}
+void avg()                                          // function definition
+{
+	int a,b,c;
+	float avg;
+	printf("enter  the a and b and c\n");
+	scanf("%d%d%d",&a,&b,&c);
+	avg=(a+b+c)/3.0;
+	printf("average is %f",avg);
+}*/
+
+
+/*#include<stdio.h>
+#include<conio.h>
+float avg();                                           // prototype
+void main()
+{
+	float r;
+	r=avg();                                           // function calling
+	printf("avg is %f",r);
+	getch();
+}
+float avg()                                          // function definition
+{
+	int a,b,c;
+	float avg;
+	printf("enter  the a and b and c\n");
+	scanf("%d%d%d",&a,&b,&c);
+	avg=(a+b+c)/3.0;
+	return avg;
+}*/
+
+
---
+++ bnm.c
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x,s;
+	cout<<"enter a no"<<endl;
+	cin>>x;
+	s=x*x;
+	cout<<"sqr is"<<s;
+	return 0;
+}
