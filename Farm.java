90fa2eb6e6d61c2ef64d885e1da95a9c6ec4206b
Nikhil <ns1043937@gmail.com>
2018-09-06 19:21:09 -0700

    farmer feb algo

---
+++ Farm.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+// problem : Farmer Feb
+
+public class Farm
+{
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int t, x, y, sum, i, j;
+        t = sc.nextInt();
+        while (t-- > 0)
+        {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            for (i = 1; i <= 1000; i++)
+            {
+                sum = x + y + i;
+                boolean flag = true;
+                for (j = 2; j <= sum / 2; j++)
+                {
+                    if (sum % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    System.out.print(i);
+                    break;
+                }
+            }
+        }
+    }
+}
+
